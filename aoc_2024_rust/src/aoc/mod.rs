67d5ef092c365be9b7ce1768288c3483969981d3
Jeremy Savor <jeremy.savor@gmail.com>
2024-12-10 23:40:14 +0100

    Add ability to run any implemented day

--- aoc_2024_rust/src/aoc/mod.rs
+++ aoc_2024_rust/src/aoc/mod.rs
@@ -1,22 +1,26 @@
-pub mod input;
 pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
 pub mod day5;
 pub mod day6;
+pub mod input;
+
+static DAYS: &[fn(); 6] = &[
+    day1::run,
+    day2::run,
+    day3::run,
+    day4::run,
+    day5::run,
+    day6::run,
+];
 
 pub fn run_all() {
-    let days = vec![
-        day1::run,
-        day2::run,
-        day3::run,
-        day4::run,
-        day5::run,
-        day6::run,
-    ];
-
-    for day in days {
+    for day in DAYS.clone() {
         day();
     }
+}
+
+pub fn run(day: usize) {
+    DAYS[day - 1]()
 }
--- aoc_2024_rust/src/main.rs
+++ aoc_2024_rust/src/main.rs
@@ -1,5 +1,13 @@
 mod aoc;
+use std::env;
 
 fn main() {
-    aoc::run_all()
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    if args.len() > 1 {
+        let day = &args[1].parse::<usize>().unwrap();
+        aoc::run(*day);
+    } else {
+        aoc::run_all()
+    }
 }
