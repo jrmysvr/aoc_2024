fa42c061648645c00472f3c600ac5b8d7dbaf554
Jeremy Savor <jeremy.savor@gmail.com>
2024-12-11 23:17:58 +0100

    [rust] Solve day 11 part 1

--- aoc_2024_rust/src/aoc/mod.rs
+++ aoc_2024_rust/src/aoc/mod.rs
@@ -1,4 +1,5 @@
 pub mod day1;
+pub mod day11;
 pub mod day2;
 pub mod day3;
 pub mod day4;
@@ -6,17 +7,26 @@ pub mod day5;
 pub mod day6;
 pub mod input;
 
-static DAYS: &[fn(); 6] = &[
+static DAYS: &[fn()] = &[
     day1::run,
     day2::run,
     day3::run,
     day4::run,
     day5::run,
     day6::run,
+    // Day 7
+    || {},
+    // Day 8
+    || {},
+    // Day 9
+    || {},
+    // Day 10
+    || {},
+    day11::run,
 ];
 
 pub fn run_all() {
-    for day in DAYS.clone() {
+    for day in DAYS {
         day();
     }
 }
