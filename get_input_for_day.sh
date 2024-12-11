ef76cc630af30d1205400029cf70a90dbf9e2195
Jeremy Savor <jeremy.savor@gmail.com>
2024-12-11 17:21:25 +0100

    improve getter script

--- get_input_for_day.sh
+++ get_input_for_day.sh
@@ -5,8 +5,18 @@ then
     echo "Usage: $0 <day number>"
     exit 1
 fi
+
+if [ -z "$AOC_SESSION_TOKEN" ]
+then
+    echo "AOC_SESSION_TOKEN is not set"
+    exit 1
+fi
+
 mkdir -p inputs
 curl \
+    --fail \
+    --silent \
+    --show-error \
     --cookie "$AOC_SESSION_TOKEN" \
     "https://adventofcode.com/2024/day/$day/input" \
     > "inputs/day$day.txt"
