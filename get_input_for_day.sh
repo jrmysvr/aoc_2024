8d859540eea35c40dbe078e59e40f0d7cd6a5a61
Jeremy Savor <jeremy.savor@gmail.com>
2024-12-12 20:24:10 +0100

    Source the local `.aoc_session_file`

--- get_input_for_day.sh
+++ get_input_for_day.sh
@@ -1,14 +1,16 @@
 day="$1"
 
-if [ -z "$day" ]
-then
-    echo "Usage: $0 <day number>"
-    exit 1
-fi
+. .aoc_session_token
 
 if [ -z "$AOC_SESSION_TOKEN" ]
 then
     echo "AOC_SESSION_TOKEN is not set"
+    exit 1
+fi
+
+if [ -z "$day" ]
+then
+    echo "Usage: $0 <day number>"
     exit 1
 fi
 
