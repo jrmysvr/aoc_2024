13f3ea9c67fc3675b8b794de901a81235a00e9c5
Jeremy Savor <jeremy.savor@gmail.com>
2024-12-22 11:07:38 +0100

    [rust] Solve day 15 part 2

--- aoc_2024_rust/src/aoc/day15.rs
+++ aoc_2024_rust/src/aoc/day15.rs
@@ -123,6 +123,45 @@ fn move_thing(thing: &Thing, grid: &Grid, direction: Direction) -> Option<(Thing
     None
 }
 
+fn move_thing_2(thing: &Thing, grid: &Grid, direction: Direction) -> Option<(Thing, Grid)> {
+    if in_bounds(move_loc(thing.loc(), direction), grid) {
+        let thing_to_push = get_adjacent_to(thing, direction, grid);
+        match thing_to_push.c {
+            // Move!
+            '.' => {
+                let moved_thing = thing.move_to(thing_to_push.loc());
+                let mut new_grid = grid.clone();
+                new_grid[thing.i as usize][thing.j as usize] = '.';
+                new_grid[moved_thing.i as usize][moved_thing.j as usize] = moved_thing.c;
+                return Some((moved_thing, new_grid));
+            }
+            // Need to push stuff
+            '[' | ']' => match direction {
+                '<' | '>' => {
+                    if let Some((_, grid)) = move_thing_2(&thing_to_push, grid, direction) {
+                        return move_thing_2(thing, &grid, direction);
+                    }
+                }
+                '^' | 'v' => {
+                    let lr = if thing_to_push.c == '[' { '>' } else { '<' };
+                    let other_side = get_adjacent_to(&thing_to_push, lr, grid);
+                    if let Some((_, grid)) = move_thing_2(&other_side, &grid, direction) {
+                        if let Some((_, grid)) = move_thing_2(&thing_to_push, &grid, direction) {
+                            return move_thing_2(thing, &grid, direction);
+                        }
+                    }
+                }
+                _ => {}
+            },
+            // At a wall, so do nothing
+            '#' => {}
+            _ => {}
+        }
+    }
+
+    None
+}
+
 fn get_thing_at(loc: Loc, grid: &Grid) -> Thing {
     let loc0 = loc.0 as usize;
     let loc1 = loc.1 as usize;
@@ -146,12 +185,59 @@ fn solve_part1(input: &String) -> String {
         .to_string()
 }
 
-fn solve_part2(_input: &String) -> String {
-    String::new()
+fn scale_grid(grid: &Grid) -> Grid {
+    let mut new_grid = Grid::new();
+    for row in grid {
+        let mut new_row = Row::new();
+        for elem in row {
+            match elem {
+                '#' => {
+                    new_row.push('#');
+                    new_row.push('#');
+                }
+                '.' => {
+                    new_row.push('.');
+                    new_row.push('.');
+                }
+                'O' => {
+                    new_row.push('[');
+                    new_row.push(']');
+                }
+                '@' => {
+                    new_row.push('@');
+                    new_row.push('.');
+                }
+                _ => panic!("Unexpected element: {elem}"),
+            }
+        }
+        new_grid.push(new_row);
+    }
+
+    new_grid
+}
+
+fn solve_part2(input: &String) -> String {
+    let (_, grid, moves) = parse_input(input);
+    let mut grid = scale_grid(&grid);
+    let mut robot = find_things_in(&grid, '@')[0].clone();
+
+    for direction in moves {
+        if let Some(robot_grid) = move_thing_2(&robot, &grid, direction) {
+            (robot, grid) = robot_grid;
+        }
+    }
+
+    let boxes = find_things_in(&grid, '[');
+    boxes
+        .iter()
+        .map(|b| 100 * b.i + b.j)
+        .fold(0, |acc, gps_coor| acc + gps_coor)
+        .to_string()
 }
 
 #[cfg(test)]
 mod test {
+    use itertools::Itertools;
     use super::*;
 
     const INPUT: &[&str] = &[
@@ -188,10 +274,30 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
 ########
 
 <^^>>>vv<v>>v<<",
+        "
+#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^",
     ];
 
     fn get_input(ix: usize) -> String {
         String::from(INPUT[ix].trim())
+    }
+
+    fn render_grid(grid: &Grid) {
+        println!("");
+        println!(
+            "{}",
+            grid.into_iter()
+                .map(|row| row.into_iter().collect::<String>())
+                .join("\n")
+        );
     }
 
     #[test]
@@ -274,7 +380,62 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
     }
 
     #[test]
+    fn test_push_stuff_2() {
+        let (_, grid, _) = parse_input(&get_input(2));
+        let mut grid = scale_grid(&grid);
+        let mut robot = find_things_in(&grid, '@')[0].clone();
+
+        render_grid(&grid);
+        let robot_grid = move_thing_2(&robot, &grid, '<');
+        let Some((r, g)) = robot_grid else {
+            panic!("Unexpected")
+        };
+        robot = r;
+        grid = g;
+
+        assert_eq!(robot.i, 3);
+        assert_eq!(robot.j, 9);
+        let loc = (robot.i - 1, robot.j);
+        let w = Thing::new('.', loc.0, loc.1);
+        assert_eq!(get_thing_at(loc, &grid), w);
+        render_grid(&grid);
+
+        for direction in vec!['v', 'v', '<', '<', '^'] {
+            let Some((r, g)) = move_thing_2(&robot, &grid, direction) else {
+                panic!("Unexpected")
+            };
+            robot = r;
+            grid = g;
+            render_grid(&grid);
+        }
+        assert!(move_thing_2(&robot, &grid, '^').is_none());
+        render_grid(&grid);
+        for direction in vec!['<', '<', '^', '^'] {
+            let Some((r, g)) = move_thing_2(&robot, &grid, direction) else {
+                panic!("Unexpected")
+            };
+
+            robot = r;
+            grid = g;
+            render_grid(&grid);
+        }
+        assert!(move_thing_2(&robot, &grid, '^').is_none());
+        render_grid(&grid);
+    }
+
+    #[test]
+    fn test_scale_grid() {
+        let (_, grid, _) = parse_input(&get_input(2));
+        let grid = scale_grid(&grid);
+        assert_eq!(grid.len(), 7);
+        assert_eq!(grid[0].len(), 14);
+        assert_eq!(grid[3].len(), 14);
+        assert_eq!(grid[3][6], '[');
+        assert_eq!(grid[3][7], ']');
+    }
+
+    #[test]
     fn test_full_part2() {
-        assert_eq!(solve_part2(&get_input(0)), "");
+        assert_eq!(solve_part2(&get_input(0)), "9021");
     }
 }
